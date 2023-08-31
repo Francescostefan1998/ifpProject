1474e8be0fac2d3cfa62e52bfb94604d36bf06a2
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-08-31 17:21:42 +0200

    complete all the servlet

--- src/main/java/it/betacom/controller/GetCourses.java
+++ src/main/java/it/betacom/controller/GetCourses.java
@@ -1,11 +1,22 @@
 package it.betacom.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import it.betacom.connection.Database;
 
 /**
  * Servlet implementation class GetCourses
@@ -13,27 +24,55 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/GetCourses")
 public class GetCourses extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public GetCourses() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public GetCourses() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		JSONArray jsonArray = new JSONArray();
+		try {
+			Connection con = Database.getConnection();
+			String sql = "select * from courses";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+
+			while (rs.next()) {
+				JSONObject jsonObject = new JSONObject();
+				for (int i = 1; i <= columnCount; i++) {
+					String columnName = rsmd.getColumnName(i);
+					Object value = rs.getObject(columnName);
+					jsonObject.put(columnName, value);
+				}
+				jsonArray.put(jsonObject);
+			}
+			response.setContentType("application/json");
+			response.getWriter().write(jsonArray.toString());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
