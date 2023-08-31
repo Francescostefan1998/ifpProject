62b1e80dbe450f0a5f3de6a79d3522878e3f38af
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2023-08-31 12:05:14 +0200

    initialization

---
+++ src/main/java/it/betacom/controller/GetBranches.java
@@ -0,0 +1,82 @@
+package it.betacom.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import it.betacom.connection.Database;
+
+/**
+ * Servlet implementation class GetBranches
+ */
+@WebServlet("/GetBranches")
+public class GetBranches extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public GetBranches() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		JSONArray jsonArray = new JSONArray();
+		try {
+			Connection con = Database.getConnection();
+			String sql = "select * from branches";
+
+			PreparedStatement ps = con.prepareStatement(sql);
+
+			ResultSet rs = ps.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int columnCount = rsmd.getColumnCount();
+
+			while (rs.next()) {
+				JSONObject jsonObject = new JSONObject();
+				for (int i = 1; i <= columnCount; i++) {
+					String columnName = rsmd.getColumnName(i);
+					Object value = rs.getObject(columnName);
+					jsonObject.put(columnName, value);
+				}
+				jsonArray.put(jsonObject);
+			}
+
+			response.setContentType("application/json");
+			response.getWriter().write(jsonArray.toString());
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
